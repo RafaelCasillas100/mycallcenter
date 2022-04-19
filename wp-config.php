246056aaf44ad10915ee825de655aafb869abaf5
“Rafael <rafachido12@hotmail.comgit config --global user.name Rafael Casillasgit config --global user.email rafachido12@hotmail.com>
2022-04-19 00:43:08 -0500

    wp-config changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mycallce_tp' );
+define( 'DB_NAME', 'wordpress_db' );
 
 /** MySQL database username */
-define( 'DB_USER', 'mycallce_tp' );
+define( 'DB_USER', 'wordpress_usr' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '7fSB3Tp@-2' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
